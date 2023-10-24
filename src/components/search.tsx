b0cefef97ddc2dc8279c5c8acee95edd5d581270
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-24 11:20:23 +0300

    feat: add search

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,13 +1,46 @@
 import { Component, ReactNode } from 'react';
+import { ls } from '../helpers/local-storage';
 
-export class Search extends Component {
+type Props = {
+  onSubmit: (v: string) => void;
+};
+
+type State = {
+  search: string;
+};
+
+export class Search extends Component<Props, State> {
+  state: Readonly<State> = {
+    search: '',
+  };
+
+  componentDidMount(): void {
+    const search = ls.getSearch();
+    this.setState({ search });
+    // this.props.onSubmit(search);
+  }
+
+  handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    ls.saveSearch(this.state.search);
+    this.props.onSubmit(this.state.search);
+  };
+
   render(): ReactNode {
     return (
       <div
         style={{ display: 'flex', padding: '10px', justifyContent: 'center' }}
       >
-        <form style={{ display: 'flex', gap: '5px' }}>
-          <input type="text" placeholder="Search..." />
+        <form
+          onSubmit={(e) => this.handleSubmit(e)}
+          style={{ display: 'flex', gap: '5px' }}
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            value={this.state.search}
+            onChange={(e) => this.setState({ search: e.target.value })}
+          />
           <button type="submit" style={{ padding: '4px' }}>
             Search
           </button>
--- src/components/searchable-list.tsx
+++ src/components/searchable-list.tsx
@@ -2,22 +2,26 @@ import { Component, ReactNode } from 'react';
 import { List } from './list';
 import { Search } from './search';
 import { ICrypto, getCryptos } from '../api/get-cryptos';
+import { ls } from '../helpers/local-storage';
 
 type State = {
-  search: string;
   cryptos: ICrypto[];
 };
 
 export class SearchableList extends Component<{}, State> {
   state: Readonly<State> = {
-    search: '',
     cryptos: [],
   };
 
-  componentDidMount(): void {
-    getCryptos().then((res) =>
+  fetchCryptos(search?: string) {
+    getCryptos(search).then((res) =>
       this.setState({ ...this.state, cryptos: res.data })
     );
+  }
+
+  componentDidMount(): void {
+    const search = ls.getSearch();
+    this.fetchCryptos(search);
   }
 
   render(): ReactNode {
@@ -30,7 +34,7 @@ export class SearchableList extends Component<{}, State> {
           textAlign: 'center',
         }}
       >
-        <Search />
+        <Search onSubmit={(v) => this.fetchCryptos(v)} />
         <List items={this.state.cryptos} />
       </div>
     );
