957074353cd381eca94b5652d61e1c17f51123f0
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-26 11:17:50 +0300

    feat: add links to details

--- src/components/list-item.tsx
+++ src/components/list-item.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { ICrypto } from '../api/get-cryptos';
 import { showPrice } from '../helpers/show-price';
 
@@ -11,21 +12,28 @@ export function ListItem({
   return (
     <li
       style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '8px',
-        padding: '15px',
         border: '1px solid black',
         background: 'gray',
         color: 'white',
         ...styles,
       }}
     >
-      <p>Rank: {crypto.rank}</p>
-      <p style={{ fontSize: '1.6rem' }}>{crypto.name}</p>
-      <p>Price: ${showPrice(crypto.priceUsd)}</p>
+      <Link
+        to={`details/${crypto.id}`}
+        style={{
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '8px',
+          padding: '15px',
+        }}
+      >
+        <p>Rank: {crypto.rank}</p>
+        <p style={{ fontSize: '1.6rem' }}>{crypto.name}</p>
+        <p>Price: ${showPrice(crypto.priceUsd)}</p>
+      </Link>
     </li>
   );
 }
